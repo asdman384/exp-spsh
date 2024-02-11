015bb2e003d5c38a11b274afe42028e1708d3cb1
asdman384 <13420499+asdman384@users.noreply.github.com>
2024-02-11 14:38:45 +0200

    fix snow fun

--- src/fun/snow/snow.component.ts
+++ src/fun/snow/snow.component.ts
@@ -10,6 +10,7 @@ import { Component } from '@angular/core';
 export class SnowComponent {
   protected enabled = new Date().getMonth() === 1 && new Date().getDate() === 14;
 
+  // in accordance with scss `$total` variable
   protected numbers = Array(50)
     .fill(1)
     .map(() => Math.random() > 0.5);
